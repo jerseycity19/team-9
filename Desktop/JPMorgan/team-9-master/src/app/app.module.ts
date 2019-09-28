6e90bedcad094714ed408a49be29e9f22a463be4
Oluwafemi Odekunle <femiodekunle@ymail.com>
2019-09-28 01:13:04 -0400

    test

---
+++ Desktop/JPMorgan/team-9-master/src/app/app.module.ts
@@ -0,0 +1,71 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+
+import { AppComponent } from './app.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { IconsComponent } from './icons/icons.component';
+import { MapsComponent } from './maps/maps.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+// import {
+//   AgmCoreModule
+// } from '@agm/core';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBURqCqrg381QkI10LlmDw18_4NuAYo71w",
+  authDomain: "codeforgood9.firebaseapp.com",
+  databaseURL: "https://codeforgood9.firebaseio.com",
+  projectId: "codeforgood9",
+  storageBucket: "",
+  messagingSenderId: "483495702605",
+  appId: "1:483495702605:web:406deed27356e3277cec04"
+};
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    ComponentsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule, // storage
+    RouterModule,
+    AppRoutingModule,
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    // })
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent,
+    MainLayoutComponent,
+    MainComponent,
+    AboutComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Desktop/JPMorgan/team-9-master/src/app/layouts/main-layout/main-layout.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainLayoutRoutingModule } from './main-layout-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MainLayoutRoutingModule
+  ]
+})
+export class MainLayoutModule { }
